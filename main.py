7275f0a3507c1d4bc00292f06e70708959451d17
Sivaramakrishnan Sankarapandian <scelesticsiva@gmail.com>
2018-05-27 13:53:58 -0400

    added regularized model

--- main.py
+++ main.py
@@ -3,13 +3,18 @@
 """
 from data.load_data import data_loader
 from models.basic_model import base_model
+from models.regularized_model import regularized_model
 import tensorflow as tf
 import numpy as np
 TRAIN_FILE_NAME = "/Users/siva/Documents/falconai/training.txt"
 
 def train(base_model_config):
     data,labels,train_op,val_op = data_loader(TRAIN_FILE_NAME).data_loader_train(base_model_config["batch_size"])
-    model_obj = base_model(base_model_config,data,labels)
+
+    #--------------- Different models ----------------#
+    #model_obj = base_model(base_model_config,data,labels)
+    model_obj = regularized_model(base_model_config,data,labels)
+    # ------------------------------------------------#
     model = model_obj.inference()
     init = tf.global_variables_initializer()
 
@@ -38,12 +43,13 @@ def train(base_model_config):
                 print("Val Accuracy:", np.mean(TEST_ACCURACY_LIST), "|", "Val Loss:", np.mean(TEST_LOSS_LIST))
 
 if __name__ == "__main__":
-    base_model_config = {"epochs": 10, \
-                         "batch_size": 10, \
+    base_model_config = {"epochs": 30, \
+                         "batch_size": 52, \
                          "optimizer": tf.train.AdamOptimizer, \
                          "lr": 0.001, \
+                         "lambda_":0.001,\
                          "loss": tf.nn.softmax_cross_entropy_with_logits, \
-                         "dropout": 0.5
+                         "dropout": 0.8
                          }
 
     train(base_model_config)
--- models/regularized_model.py
+++ models/regularized_model.py
@@ -3,7 +3,7 @@
 """
 import tensorflow as tf
 
-class base_model(object):
+class regularized_model(object):
     def __init__(self,config,x_,y_):
         self.epochs = config["epochs"]
         self.batch_size = config["batch_size"]
@@ -41,12 +41,12 @@ class base_model(object):
                 conv_2_w = self.weights_("conv_2w",[3,3,32,64])
                 conv_2_b = self.biases_("conv_2b",[64])
 
-            with tf.name_scope("conv_3"):
-                conv_3_w = self.weights_("conv_3w",[3,3,64,128])
-                conv_3_b = self.biases_("conv_3b",[128])
+            #with tf.name_scope("conv_3"):
+            #    conv_3_w = self.weights_("conv_3w",[3,3,64,128])
+            #    conv_3_b = self.biases_("conv_3b",[128])
 
             with tf.name_scope("full_1"):
-                full_1_w = self.weights_("full_1w",[28*28*128,128])
+                full_1_w = self.weights_("full_1w",[56*56*64,128])
                 full_1_b = self.biases_("full_1b",[128])
 
             with tf.name_scope("full_2"):
@@ -62,28 +62,21 @@ class base_model(object):
             max_pool_conv_1 = self.max_pool(conv_1)
             conv_2 = tf.nn.dropout(tf.nn.relu(tf.nn.bias_add(self.conv_2d(max_pool_conv_1, conv_2_w), conv_2_b)),self.keep_probability)
             max_pool_conv_2 = self.max_pool(conv_2)
-            conv_3 = tf.nn.dropout(tf.nn.relu(tf.nn.bias_add(self.conv_2d(max_pool_conv_2, conv_3_w), conv_3_b)),self.keep_probability)
-            max_pool_conv_3 = self.max_pool(conv_3)
-            reshaped_last_conv = tf.reshape(max_pool_conv_3, (-1, 28*28*128))
+            #conv_3 = tf.nn.dropout(tf.nn.relu(tf.nn.bias_add(self.conv_2d(max_pool_conv_2, conv_3_w), conv_3_b)),self.keep_probability)
+            #max_pool_conv_3 = self.max_pool(conv_3)
+            reshaped_last_conv = tf.reshape(max_pool_conv_2, (-1, 56*56*64))
             full_1 = tf.nn.dropout(tf.nn.relu(tf.nn.bias_add(tf.matmul(reshaped_last_conv, full_1_w), full_1_b)),self.keep_probability)
             full_2 = tf.nn.dropout(tf.nn.relu(tf.nn.bias_add(tf.matmul(full_1, full_2_w), full_2_b)),self.keep_probability)
             self.output = tf.nn.bias_add(tf.matmul(full_2,full_3_w),full_3_b)
 
-        self.calculated_loss = tf.reduce_mean(self.loss(logits=self.output, labels=self.y))
+        trainable_vars = tf.trainable_variables()
+        self.regularized_loss = tf.add_n([tf.nn.l2_loss(i) for i in trainable_vars if "b" not in i.name],name = "regularized_loss")*self.lambda_
+
+        self.calculated_loss = tf.reduce_mean(self.loss(logits=self.output, labels=self.y)) + self.regularized_loss
         self.calculated_acc = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(input=self.y,axis = 1),tf.argmax(input=self.output,axis = 1)),tf.float32))
         self.optimizer = self.optimizer_(self.lr).minimize(self.calculated_loss)
 
         return {"inputs":[self.x,self.y],"output":self.output,"optimizer":self.optimizer,"acc":self.calculated_acc,"loss":self.calculated_loss}
-
-    # def calculate_loss(self):
-    #     self.calculated_loss = tf.reduce_mean(self.loss(logits = self.output,labels = self.y))
-    #
-    # def calculate_accuracy(self):
-    #     self.calculated_acc = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(self.output,axis = 1),tf.argmax(self.y,axis = 1)),tf.float32))
-    #
-    # def optimize(self):
-    #     self.optimizer = self.optimizer_(self.lr).minimize(self.calculated_loss)
-
 
 
 
